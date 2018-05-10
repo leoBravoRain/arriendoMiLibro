b71382a2357dc738dc884ab7c6895e460c7d30ac
Gian <gian.bravo@alumnos.usm.cl>
2018-05-10 14:48:49 -0300

    version estable luego de debugueo de rotacion de imagen

--- arriendoMiLibro/arriendoMiLibro/funcionesGlobales.py
+++ arriendoMiLibro/arriendoMiLibro/funcionesGlobales.py
@@ -5,24 +5,20 @@ def imageAutorotate(foto):
 
     with Image.open(foto) as image:
 
-        image.show()
-
         file_format = image.format
-
-        print file_format
 
         # Se chequea si es JPEG, ya que solo se puede extraer el exif solo desde JPEG (Usando PIL)
         if file_format == "JPEG":
 
             exif = image._getexif()
 
-            print exif
-
             # image.thumbnail((1667, 1250), resample=Image.ANTIALIAS)
 
             # if image has exif data about orientation, let's rotate it
             orientation_key = 274 # cf ExifTags
+
             if exif and orientation_key in exif:
+                
                 orientation = exif[orientation_key]
 
                 rotate_values = {
@@ -33,8 +29,5 @@ def imageAutorotate(foto):
 
                 if orientation in rotate_values:
 
-                    print "se rota la imagen"
                     image = image.transpose(rotate_values[orientation])
-
-
                     image.save(foto.path, file_format)
